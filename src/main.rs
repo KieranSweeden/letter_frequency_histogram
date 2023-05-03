ca637c29218f6dc477ba44b6615da2227db2f6f1
kieran <kieransweeden@gmail.com>
2023-05-03 19:50:24 +0100

    Prompt & validation of user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::io;
+
+struct UserInput {
+    content: String,
+}
+
+impl UserInput {
+    fn validate(&self) -> bool {
+        !&self.content.is_empty()
+    }
+}
+
+impl UserInput {
+    fn prompt() -> UserInput {
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).unwrap();
+        UserInput {
+            content: user_input.trim().to_string(),
+        }
+    }
+}
+
+fn run() {
+    loop {
+        let user_input = UserInput::prompt();
+        let is_valid = user_input.validate();
+        if !is_valid {
+            println!("Provided input was not valid, try again.");
+            continue;
+        }
+    }
+}
+
+fn intro() {
+    println!("========");
+    println!("Welcome!");
+    println!("========");
+}
+
 fn main() {
-    println!("Hello, world!");
+    intro();
+    run();
 }
